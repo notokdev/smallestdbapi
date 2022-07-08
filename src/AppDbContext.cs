d90fdc1151f7754bfe1e1262e0a4e5376185659f
Jordan Campos Leite <jordan.leite@philips.com>
2022-07-08 15:16:39 -0300

    first commit

---
+++ src/AppDbContext.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace SmallestDbApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
